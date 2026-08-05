87125f1786d5da003dd6fed321786cb1949b8ab9
742221 <hykqo@naver.com>
2026-08-05 16:01:46 +0900

    기본형과참조형: 메서드 호출

---
+++ src/main/java/javaBasic/ref/MethodChange2.java
@@ -0,0 +1,15 @@
+package javaBasic.ref;
+
+public class MethodChange2 {
+
+    public static void main(String[] args) {
+        Data dataA = new Data();
+        dataA.value = 10;
+        System.out.println("메서드 호출 전 : a = " + dataA.value); //10
+        changePrimitive(dataA);
+        System.out.println("메서드 호출 후 : a = " + dataA.value); //20
+    }
+    static void changePrimitive(Data dataX){
+        dataX.value = 20;
+    }
+}
