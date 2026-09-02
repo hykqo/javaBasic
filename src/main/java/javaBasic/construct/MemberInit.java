2c1829535995213c2b4bc82438f9c0ebfa4c1714
742221 <hykqo@naver.com>
2026-09-02 09:44:17 +0900

    생성자: this

--- src/main/java/javaBasic/construct/MemberInit.java
+++ src/main/java/javaBasic/construct/MemberInit.java
@@ -4,4 +4,12 @@ public class MemberInit {
     String name;
     int age;
     int grade;
+
+    void initMember(String name, int age, int grade){
+        //name = name 으로 하면 변수는 가까운 우선순서를 가지기 때문에 참조받은 변수를 사용하게된다.
+        //this = 자기자신의 인스턴스를 지칭.
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
 }
