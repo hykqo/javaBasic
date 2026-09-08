152a5942a46c4e400ebc338a84e2b4cce46b37c9
742221 <hykqo@naver.com>
2026-09-08 10:34:12 +0900

    생성자: 오버로딩과 this()

--- src/main/java/javaBasic/construct/MemberConstruct.java
+++ src/main/java/javaBasic/construct/MemberConstruct.java
@@ -5,6 +5,14 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    //추가
+    MemberConstruct(String name, int age){
+        this(name, age, 50);//변경
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50;
+    }
+
     MemberConstruct(String name, int age, int grade){
         System.out.println("create MemberConstruct - name:" + name + ", age:" + age + ", grade:" + grade);
         this.name = name;
